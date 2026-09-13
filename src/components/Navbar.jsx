2133321e39022027e3ea9c651ca1ba1d520b3cb6
Vibhuwuu <vibhorsingh388@gmail.com>
2026-09-13 11:05:17 +0530

    Improved responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <h2>Bean & Brew</h2>
+    <nav className="navbar">
+      <Link className="logo" to="/">
+        Bean & Brew ☕
+      </Link>
 
-      <Link to="/">Home</Link>
-      <Link to="/menu">Menu</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
     </nav>
   );
 }
